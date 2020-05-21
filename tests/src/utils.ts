09742970d391c0e77ec37fbe2f8b89fc5d425d74
Nikita Grishin <nikita.grishin@revolut.com>
2020-05-21 20:53:29 +0300

    trigger

--- tests/src/utils.ts
+++ tests/src/utils.ts
@@ -1,10 +1,8 @@
 const sqlite = process.env.AUTH_SQLITE_HOST as string
 const mongo = process.env.AUTH_MONGO_HOST as string
-const postgres = process.env.AUTH_POSTGRES_HOST as string
 
-export const BACKENDS = [sqlite, mongo, postgres]
+export const BACKENDS = [sqlite, mongo]
 export const NAME = {
   [sqlite]: 'sqlite',
   [mongo]: 'mongo',
-  [postgres]: 'postgres',
 } as any
