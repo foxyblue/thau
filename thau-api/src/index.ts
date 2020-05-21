ed1d98d05e5fd65e8065d5acdbea76659144c062
Nikita Grishin <nikita.grishin@revolut.com>
2020-05-21 20:43:09 +0300

    trigger

--- thau-api/src/index.ts
+++ thau-api/src/index.ts
@@ -37,7 +37,7 @@ const main = async () => {
   app.use('/users', UsersAPI)
   app.use('/configs', ConfigsAPI)
 
-  app.get('/heartbeat', (req, res) => {
+  app.get('/heartbeat', (r, res) => {
     res.send({
       service: 'thau',
       data_backend: configs.data_backend,
