2c5ffc5f158ee8c498fd7e23313f3e098f684c1c
Nikita Grishin <nikita.grishin@revolut.com>
2020-05-21 22:52:15 +0300

    Fix #12 - add sleep for a startup of postgres instance

--- tests/src/utils.ts
+++ tests/src/utils.ts
@@ -1,8 +1,8 @@
-const sqlite = process.env.AUTH_SQLITE_HOST as string
-const mongo = process.env.AUTH_MONGO_HOST as string
+const TESTABLE_DATA_BACKENDS = process.env.TESTABLE_DATA_BACKENDS as string
 
-export const BACKENDS = [sqlite, mongo]
-export const NAME = {
-  [sqlite]: 'sqlite',
-  [mongo]: 'mongo',
-} as any
+export const BACKENDS = TESTABLE_DATA_BACKENDS.split(',').map((db) => `thau-${db}:9000`)
+export const NAME = TESTABLE_DATA_BACKENDS.split(',').reduce((acc: any, db) => ({
+  ...acc,
+  [`thau-${db}:9000`]: db
+}), {})
+
