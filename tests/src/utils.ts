5bdb7d02f58945599985ae4ec05bdc8c98dba363
Nikita Grishin <nikita.grishin@revolut.com>
2020-05-21 22:54:35 +0300

    format

--- tests/src/utils.ts
+++ tests/src/utils.ts
@@ -1,8 +1,12 @@
 const TESTABLE_DATA_BACKENDS = process.env.TESTABLE_DATA_BACKENDS as string
 
-export const BACKENDS = TESTABLE_DATA_BACKENDS.split(',').map((db) => `thau-${db}:9000`)
-export const NAME = TESTABLE_DATA_BACKENDS.split(',').reduce((acc: any, db) => ({
-  ...acc,
-  [`thau-${db}:9000`]: db
-}), {})
-
+export const BACKENDS = TESTABLE_DATA_BACKENDS.split(',').map(
+  db => `thau-${db}:9000`
+)
+export const NAME = TESTABLE_DATA_BACKENDS.split(',').reduce(
+  (acc: any, db) => ({
+    ...acc,
+    [`thau-${db}:9000`]: db,
+  }),
+  {}
+)
